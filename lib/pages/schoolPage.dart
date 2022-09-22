7b363372654d7a1f8b0fe21f8f2a1a1b4c9d836c
Amanda <amandaferreirapicinini@gmail.com>
2022-09-22 00:35:45 -0300

    adição do redirecionamento para URL

--- lib/pages/schoolPage.dart
+++ lib/pages/schoolPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('https://schoolofnet.com');
 
 class SchoolPage extends StatefulWidget {
   const SchoolPage({Key? key}) : super(key: key);
@@ -11,11 +14,25 @@ class _SchoolPageState extends State<SchoolPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text('Visite:', textAlign: TextAlign.center),
+        Text('Visite:',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.displayMedium),
         ElevatedButton(
-            onPressed: () {}, child: Text('Looks like a RaisedButton'))
+            onPressed: () {
+              _launchUrl();
+            },
+            child: Text(
+              'Looks like a RaisedButton',
+            ))
       ],
     );
+  }
+}
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw 'Could not launch $_url';
   }
 }
